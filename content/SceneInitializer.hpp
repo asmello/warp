466f5cfb24d45600d073b904c344dafa69908028
Maglethong Spirr <maglethongspirr@gmail.com>
2016-06-08 21:50:37 -0300

    Trying to load coin model

--- content/SceneInitializer.hpp
+++ content/SceneInitializer.hpp
@@ -3,6 +3,9 @@
 
 #include <memory>
 
+#include <vector>
+
+
 #include "MaterialManager.hpp"
 #include "TextureManager.hpp"
 #include "ShaderManager.hpp"
@@ -50,35 +53,43 @@ public:
 		////////////////////////
 
 		// Load a shader from file
-		auto shaderID = shaderManager->createFromFile(util::resourcePath() + "vertex.glsl",
-			util::resourcePath() + "frag.glsl");
+		auto shaderID = shaderManager->createFromFile(util::resourcePath() + "vertex2.glsl",
+			util::resourcePath() + "frag2.glsl");
 
 		// Load a texture from file
-		auto textureID = textureManager->createFromFile(util::resourcePath() + "test.png");
+		auto colorTextureID = textureManager->createFromFile(util::resourcePath() + "Logo_CLM.bmp");
+		auto normalTextureID = textureManager->createFromFile(util::resourcePath() + "Logo_NRM.png");
 
 		// Create a material from shader and texture
-		auto materialID = materialManager->create(textureID, shaderID);
+		auto materialID = materialManager->create(std::vector<warp::Texture::ID>({ colorTextureID, normalTextureID }), shaderID);
 
 		// Create a mesh representation
-		auto squareID = meshManager->create();
-		if (std::shared_ptr<warp::Mesh> squareMesh = meshManager->get(squareID))
+		auto meshID = meshManager->create();
+		// TODO [Carregar FBX]
+
+		// Create a GameObject
+		auto go = scene->newGameObject();
+		go->newComponent<warp::MeshRenderer>(materialID, meshID);
+
+
+
+
+		/*
+
+		// Create the Gecko scene object
+		warp::Scene::ID geckoSceneID = sceneManager->createFromFile(util::resourcePath() + "LogoLP_MDL.fbx", geckoMaterialID);
+
+
+		// Adjust Gecko
+		if (std::shared_ptr<warp::GameObject> rootObject = gameObjectManager->get(warp::GameObject::ID(1)))
 		{
-			squareMesh->setVertices({
-				-0.8f, 0.8f, 0.0f,   0.0f, 0.0f, 1.0f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
-				-0.4f, 0.8f, 0.0f,   0.0f, 0.0f, 1.0f,  1.0f, 0.0f, 0.0f,  1.0f, 0.0f,
-				-0.8f, 0.4f, 0.0f,   0.0f, 0.0f, 1.0f,  1.0f, 0.0f, 0.0f,  0.0f, 1.0f,
-				-0.4f, 0.4f, 0.0f,   0.0f, 0.0f, 1.0f,  1.0f, 0.0f, 0.0f,  1.0f, 1.0f
-			});
-			squareMesh->setElements({
-				0, 2, 1,
-				2, 3, 1
-			});
-			squareMesh->load();
+			rootObject->getTransform()->scale(0.015);
+			rootObject->getTransform()->rotate(-90, 0, 1, 0);
+			rootObject->getTransform()->translate(0, -1.25, 0);
 		}
 
-		// Create a mesh object
-		auto go3 = scene->newGameObject();
-		go3->newComponent<warp::MeshRenderer>(materialID, squareID);
+		*/
+
 	}
 };
 
