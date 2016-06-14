680f0940cbaa83bddf2f46294dee802b38f939b2
André Sá de Mello <asmello.br@gmail.com>
2016-06-14 02:49:31 -0300

    Add speed to Material

--- src/Material.cpp
+++ src/Material.cpp
@@ -9,19 +9,19 @@ using namespace warp;
 template<> ResourceManager<Material> * Object<Material>::manager = MaterialManager::getInstance();
 
 Material::Material(Texture::ID texture, Shader::ID shader)
-: shaderID(shader)
+: shaderID(shader), speed(0.0f)
 {
     textureIDs.push_back(texture);
 }
 
 Material::Material(std::vector<Texture::ID> textures, Shader::ID shader)
-: shaderID(shader), textureIDs(textures)
+: shaderID(shader), textureIDs(textures), speed(0.0f)
 {
     
 }
 
 Material::Material(std::initializer_list<Texture::ID> textures, Shader::ID shader)
-: shaderID(shader), textureIDs(textures)
+: shaderID(shader), textureIDs(textures), speed(0.0f)
 {
     
 }
@@ -32,5 +32,9 @@ void Material::bind()
     for (size_t i = 0; i < textureIDs.size(); ++i)
     {
         TextureManager::getInstance()->setActive(textureIDs[i], i);
+        // Send speed to shader
+        if (std::shared_ptr<Shader> activeShader = ShaderManager::getInstance()->getActive()) {
+            glUniform1f(activeShader->getUniformLocation("u_speed"), speed);
+        }
     }
 }
